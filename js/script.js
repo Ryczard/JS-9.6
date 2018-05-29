275089636c94c647e7716c775f79c0b02e05d64c
Konrad Ryczek <konradryczek92@gmail.com>
2018-05-29 09:43:33 +0200

    Update index.html content

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@ var list = document.getElementById('list'),
 
 add.addEventListener('click', function() {
 	var element = document.createElement('li');
-	element.innerHTML = 'item' + document.getElementsByTagName('li').length;
+	element.innerHTML = 'item ' + document.getElementsByTagName('li').length;
 	list.appendChild(element);
 });
